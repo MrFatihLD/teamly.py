b7e70a4c0f261195bc7a2fb84244c637d4fc204e
Fatih Kuloglu <fatihkuloglu@proton.me>
2025-06-30 19:58:01 +0200

    created event() function; created _LoopSentinel for empty self.loop object

--- teamly/client.py
+++ teamly/client.py
@@ -12,26 +12,46 @@ T = TypeVar('T')
 Coro = Coroutine[Any,Any, T]
 CoroT = TypeVar('CoroT', bound=Callable[..., Coro[Any]])
 
+class _LoopSentinel:
+    __slots__ = ()
+
+    def __getattr__(self, attr: str) -> None:
+        msg = (
+            'loop attribute cannot be accessed in non-async contexts. '
+            'Consider using either an asynchronous main function and passing it to asyncio.run or '
+            'using asynchronous initialisation hooks such as Client.setup_hook'
+        )
+        raise AttributeError(msg)
+
+_loop: Any = _LoopSentinel()
+
+
 class Client:
 
     def __init__(self) -> None:
-        self.loop: asyncio.AbstractEventLoop = None  # type: ignore
+        self.loop: asyncio.AbstractEventLoop = _loop
 
-    def run(self, token: str):
+    def run(self, token: str) -> None:
         logger.debug('Your token is "{}"', token)
 
         async def runner():
             logger.debug("starting coroutine")
             async with self:
-                await asyncio.sleep(2)
+                await asyncio.sleep(2) #temporaly
 
         try:
             asyncio.run(runner())
         except KeyboardInterrupt:
             pass
 
-    def event(self, coro: CoroT,) -> CoroT: #type: ignore
-        pass
+    def event(self, coro: CoroT,) -> CoroT:
+
+        if not asyncio.iscoroutinefunction(coro):
+            raise TypeError("event registered must be a coroutine function")
+
+        setattr(self, coro.__name__, coro)
+        logger.debug("\"{}\" has successfully been registered as an event",coro.__name__)
+        return coro
 
     async def __aenter__(self):
         logger.debug("Entering context...")
