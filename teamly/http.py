770938516cffe3b36cc17e45dd1c28f56f95ef53
Fatih Kuloglu <fatihkuloglu@proton.me>
2025-07-01 13:34:42 +0200

    created close() and self.loop object; added debug loggers

--- teamly/http.py
+++ teamly/http.py
@@ -1,9 +1,11 @@
+import asyncio
 import aiohttp
 import json
 
 from .utils import MISSING
 from typing import Any
 from urllib.parse import quote
+from loguru import logger
 
 class Route:
     BASE_URL = "https://api.teamly.one/api/v1"
@@ -22,16 +24,24 @@ class Route:
 
 class HTTPClient:
 
-    def __init__(self) -> None:
+    def __init__(self, loop: asyncio.AbstractEventLoop) -> None:
         self._session: aiohttp.ClientSession = MISSING
         self.token = None
+        self.loop: asyncio.AbstractEventLoop = loop
 
     async def static_login(self, token: str):
+        logger.debug("static logging...")
+
         self.token = token
         self._session = aiohttp.ClientSession()
 
+    async def close(self):
+        logger.debug("closing client session...")
+        await self._session.close()
 
     async def ws_connect(self):
+        logger.debug("creating ws connect...")
+
         kwargs = {
             "timeout": 30,
             "max_msg_size": 0,
@@ -42,7 +52,7 @@ class HTTPClient:
 
         return await self._session.ws_connect(url="wss://api.teamly.one/api/v1/ws", **kwargs)
 
-    async def request(self, route: Route, **kwargs) -> aiohttp.ClientResponse:
+    async def request(self, route: Route, **kwargs) -> Any:
         method = route.method
         url = route.url
 
@@ -56,4 +66,10 @@ class HTTPClient:
             headers["Content-Type"] = "application/json"
             kwargs['data'] = json.dumps(kwargs.pop('json'))
 
-        return await self._session.request(method, url, **kwargs)
+        kwargs["headers"] = headers
+
+        try:
+            logger.debug("making request...")
+            return await self._session.request(method, url, **kwargs)
+        except Exception as e:
+            logger.error("Exception error: {}",e)
