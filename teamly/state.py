a31eefb7fa6ded4c229806ec53392ed0eb15dfbe
Fatih Kuloglu <fatihkuloglu@proton.me>
2025-07-03 09:33:40 +0200

    created channel.py and state.py

---
+++ teamly/state.py
@@ -0,0 +1,20 @@
+
+import inspect
+
+from .http import HTTPClient
+from typing import Dict, Callable, Any
+
+class ConnectionState:
+
+    def __init__(self, dispatch: Callable[...,Any],http: HTTPClient) -> None:
+        self.http: HTTPClient = http
+        self.dispatch: Callable[...,Any] = dispatch
+
+        self.parsers: Dict[str, Callable[[Any], None]]
+        self.parsers = parsers = {}
+        for attr, func in inspect.getmembers(self):
+            if attr.startswith('parse_'):
+                parsers[attr[6:].upper()] = func
+
+    def parse_ready(self):
+        self.dispatch("ready")
