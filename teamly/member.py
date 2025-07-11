b837bdd49b79b3a4b984e6f58279c775da4ab3e3
Fatih Kuloglu <fatihkuloglu@proton.me>
2025-07-11 20:03:37 +0200

    Remove id attribute from Member class

--- teamly/member.py
+++ teamly/member.py
@@ -16,7 +16,6 @@ class Member(_UserTag):
     def __init__(self,*, state: ConnectionState, data: MemberPayload) -> None:
         self._state: ConnectionState = state
         self._user: User = User(state=state, data=data)
-        self.id: str = self._user.id
         self.joined_at: str = data['joinedAt']
         self.roles: List[str] = data.get('roles', [])
         self.teamId: str = data['teamId']
